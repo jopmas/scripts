01177558c96b3286abf5474ab73845d1643b434f
João Pedro Macedo Silva <joao_lab@iMac-de-Joao.local>
2023-07-12 18:34:24 -0300

    update readme informations

---
+++ clear_plots.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm *lithology*.png
+rm *temperature*.png
+rm *density*.png
+rm *viscosity*.png
+rm *radiogenic_heat*.png
+rm *strain_*.png
+rm *pressure_*.png
--- zipper.sh
+++ zipper.sh
@@ -2,31 +2,32 @@ DIRNAME=${PWD##*/}
 
 echo "Zipping $DIRNAME directory..."
 # zip $DIRNAME.zip interfaces.txt param.txt input*_0.txt run*.sh vel*.txt run*.sh FD.out
-# zip -u $DIRNAME.zip viscosity_*.txt
-# zip -u $DIRNAME.zip heat_*.txt
-# zip -u $DIRNAME.zip density_*.txt
-# zip -u $DIRNAME.zip temperature_*.txt
-# zip -u $DIRNAME.zip time_*.txt
-# zip -u $DIRNAME.zip strain_*.txt
-# zip -u $DIRNAME.zip pressure*.txt
-# zip -u $DIRNAME.zip sp_surface_global*.txt
+ zip -u $DIRNAME.zip viscosity_*.txt
+ zip -u $DIRNAME.zip radiogenicc_heat*.txt
+ zip -u $DIRNAME.zip density_*.txt
+ zip -u $DIRNAME.zip temperature_*.txt
+ zip -u $DIRNAME.zip time_*.txt
+ zip -u $DIRNAME.zip strain_*.txt
+ zip -u $DIRNAME.zip pressure*.txt
+ zip -u $DIRNAME.zip sp_surface_global*.txt
+ zip -u $DIRNAME.zip step*.txt
 
-zip $DIRNAME'_imgs.zip' sp_surface_global*.png
-zip -u $DIRNAME'_imgs.zip' viscosity_*.png
-zip -u $DIRNAME'_imgs.zip' heat_*.png
-zip -u $DIRNAME'_imgs.zip' density_*.png
-zip -u $DIRNAME'_imgs.zip' temperature_*.png
-zip -u $DIRNAME'_imgs.zip' strain_*.png
-zip -u $DIRNAME'_imgs.zip' pressure_*.png
-zip -u $DIRNAME'_imgs.zip' litho_new_temper_*.png
-zip -u $DIRNAME'_imgs.zip' temper_mean_rm*.png
-zip -u $DIRNAME'_imgs.zip' lithology*.png
+zip $DIRNAME'_imgs.zip'    _output/*surface*.png
+zip -u $DIRNAME'_imgs.zip' _output/*viscosity_*.png
+zip -u $DIRNAME'_imgs.zip' _output/*radiogenic_heat*.png
+zip -u $DIRNAME'_imgs.zip' _output/*density_*.png
+zip -u $DIRNAME'_imgs.zip' _output/*temperature_*.png
+zip -u $DIRNAME'_imgs.zip' _output/*strain_*.png
+zip -u $DIRNAME'_imgs.zip' _output/*pressure_*.png
+zip -u $DIRNAME'_imgs.zip' _output/*temperature_anomaly*.png
+zip -u $DIRNAME'_imgs.zip' _output/*lithology*.png
 
 zip -r $DIRNAME'_videos.zip' *.mp4
+
+zip -r $DIRNAME'_gifs.zip' *.gif
 
 #zip -r $DIRNAME'_PDFs.zip' *.pdf
 
 echo "Zipping completed!"
-
 
 # rm viscosity_*.png heat_*.png density_*.png temperature_*.png strain_*.png pressure_*.png litho_new_temper_*.png
