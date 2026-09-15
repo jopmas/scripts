a83278669aba6db196896bf8e95b24679af487cb
João Macedo <joao_macmini@Mac-mini-de-Joao.local>
2026-09-15 15:13:34 -0300

    trying to fix frame_generator in Mandyoc IO

--- functions/mandyocIO.py
+++ functions/mandyocIO.py
@@ -388,7 +388,7 @@ def read_mandyoc_output(model_path, parameters_file=PARAMETERS_FNAME, datasets=t
             datasets_aux.append(scalar)
             scalars = _read_scalars(model_path, shape, steps, quantity=scalar)
             data_aux = {scalar: (dims, scalars)}
-            xr.Dataset(data_aux, coords=coords, attrs=parameters).to_netcdf(f"{model_path}/_output_{scalar}.nc")
+            xr.Dataset(data_aux, coords=coords, attrs=parameters).to_netcdf(f"{model_path}/_{scalar}.nc")
             print(f"{scalar.capitalize()} files saved.")
             del scalars
             del data_aux
@@ -442,7 +442,7 @@ def read_mandyoc_output(model_path, parameters_file=PARAMETERS_FNAME, datasets=t
 def read_datasets(model_path, datasets, save_big_dataset=False):
     empty_dataset = True
     for item in datasets:
-        dataset_aux = xr.open_dataset(f"{model_path}/_output_{item}.nc")
+        dataset_aux = xr.open_dataset(f"{model_path}/_{item}.nc")
         if (empty_dataset == True):
             dataset = dataset_aux
             empty_dataset = False
